a122a05e2dd9842470ab0474fd77d158726e4c18
devin <ddz@mit.edu>
2020-05-12 19:56:50 -0400

    fix tooltip scrolling animation

--- src/main.js
+++ src/main.js
@@ -218,7 +218,8 @@ var scrollVis = function () {
             'Russian Federation',
             'United Arab Emirates',
             'Canada',
-            'Korea, Rep.'];
+            'Korea, Rep.',
+            'Singapore'];
 
         var stacked = d3.stack()
             .keys(areaCountries)
@@ -535,10 +536,10 @@ var scrollVis = function () {
      *
      */
     function southKoreaStory() {
-        g.selectAll('.area-chart,.map')
-            .transition()
-            .duration(600)
-            .attr('opacity', 0);
+        // g.selectAll('.area-chart,.map')
+        //     .transition()
+        //     .duration(600)
+        //     .attr('opacity', 0);
 
         var svgGroup = g.selectAll('.south-korea');
         //console.log(d3.select("#usefulSelector").node().getBBox());
@@ -602,7 +603,8 @@ var scrollVis = function () {
             '2015': "2015: MERS outbreak",
             '2017': "2017: Chinese restrictions",
             '2018': "2018: PyeongChang Olympics"
-        }
+        };
+
         const specialDetails = {
             '1997': "The beginning of Korea’s rise in the entertainment industry was born in an unusual context: a financial crisis. In 1997, a major financial crisis hit Southeast and East Asia when a change in Thai fiscal policy revealed a number of asset bubbles across the region and led to massive currency devaluations, reduced imports, and political upheaval. South Korea borrowed $55 billion from the IMF, World Bank, and Asian Development Bank, a debt they were determined to re-pay as quickly as possible. Incredibly, they managed to re-pay the debt in <5 years, nearly 3 years ahead of schedule. A major part of this effort was a voluntary, national gold-collecting campaign which asked for donations from citizens to help pay back the debt. Amazingly, nearly 3.5 million Koreans (almost 25% of the population) stood in line to contribute their gold, netting $2.2 billion dollars to help reduce the debt.",
             '1998': "Korea’s economy remained weak, and the government was eager to re-define Korea’s place in the world and find new growth engines. One area in which they took a major bet was entertainment, establishing the Ministry of Culture and Tourism to invest in creative content, inject funding into the Korean Film Council, and fund the creation of 300 cultural industry departments in colleges and universities to churn out talent. Since then, the Ministry of Culture and Tourism’s budget has only grown, breaking $1 billion in 2005, reaching over $5 billion (1.4% of government spending) in 2014, and remaining 2%+ of the national budget up to today. This investment in “soft power” and cultural exports began to pay off as Korean television shows, K-pop groups, and films began to take off internationally, first in China, Japan, and other parts of Asia in the early-to-mid 2000s, and later in the U.S. and elsewhere in the late 2000s. The Korean Tourism Organization capitalized on this growth, launching tourism campaigns focused on popular shows and K-pop groups. Private entertainment agencies got involved as well, offering travel packages for tourists interested in attending concerts by their artists in 2012. The international explosion of “Gangnam Style” in 2012-13 provided another boost to the industry. With the exception of 2003, consistent and accelerating growth in tourism throughout this period demonstrated very positive ROI on this decade+ of cultural investments.",
